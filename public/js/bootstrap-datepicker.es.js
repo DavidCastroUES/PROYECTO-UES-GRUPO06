fb2946dff7244a16f2d4cc9a85aa207477772549
David José Castro Climaco <davidcastroclimaco@gmail.com>
2025-06-14 23:08:52 -0600

    Fix typos in Spanish translation for bootstrap-datepicker

--- public/js/bootstrap-datepicker.es.js
+++ public/js/bootstrap-datepicker.es.js
@@ -3,9 +3,10 @@
  * Bruno Bonamin <bruno.bonamin@gmail.com>
  */
 ;(function($){
+
 	$.fn.datepicker.dates['es'] = {
-		days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
-		daysShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
+		days: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+		daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
 		daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
 		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 		monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
